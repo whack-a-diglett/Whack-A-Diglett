1f277c2cc0263617975edabf97fd140914e139af
ASUS <hutasoitnoel@gmail.comgit config --global user.name noel>
2019-05-17 14:48:00 +0700

    update result

--- client/src/views/Result.vue
+++ client/src/views/Result.vue
@@ -50,25 +50,32 @@ background-size: cover;
 
 
 <script>
-import db from '@/api/api';
+import db from "@/api/api";
 
 export default {
   data() {
     return {
-      thisRoom: {},
+      thisRoom: {}
     };
   },
   created() {
-    db.collection('room')
+    db.collection("room")
       .doc(this.$route.params.id)
-      .onSnapshot((doc) => {
+      .onSnapshot(doc => {
         console.log(doc.data());
         this.thisRoom = doc.data();
         this.thisRoom.players.sort((a, b) => (a.score > b.score ? -1 : 1));
+        if (localStorage.username === this.thisRoom.players[0].name) {
+          var audio = new Audio("/win.mp3");
+          audio.play();
+        } else {
+          var audio = new Audio("/lose.mp3");
+          audio.play();
+        }
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
       });
-  },
+  }
 };
 </script>
--- client/src/views/Room.vue
+++ client/src/views/Room.vue
@@ -2,37 +2,63 @@
   <div id="room">
     <div style="display: flex; justify-content: center">
       <div id="moles">
-          <div style="display: flex;" class="row">
-              <span class="hitbox" @click.prevent="hit(1)"><component :is="pos1"></component></span>
-                &nbsp;
-              <span class="hitbox" @click.prevent="hit(2)"><component :is="pos2"></component></span>
-                &nbsp;
-              <span class="hitbox" @click.prevent="hit(3)"><component :is="pos3"></component></span>
-                &nbsp;
-              <span class="hitbox" @click.prevent="hit(4)"><component :is="pos4"></component></span>
-          </div>
-          <div style="display: flex;" class="row">
-              <span class="hitbox" @click.prevent="hit(5)"><component :is="pos5"></component></span>
-                &nbsp;
-              <span class="hitbox" @click.prevent="hit(6)"><component :is="pos6"></component></span>
-                &nbsp;
-              <span class="hitbox" @click.prevent="hit(7)"><component :is="pos7"></component></span>
-                &nbsp;
-              <span class="hitbox" @click.prevent="hit(8)"><component :is="pos8"></component></span>
-          </div>
-          <div style="display: flex;" class="row">
-              <span class="hitbox" @click.prevent="hit(9)"><component :is="pos9"></component></span>
-                &nbsp;
-              <span class="hitbox" @click.prevent="hit(10)"><component :is="pos10"></component></span>
-                &nbsp;
-              <span class="hitbox" @click.prevent="hit(11)"><component :is="pos11"></component></span>
-                &nbsp;
-              <span class="hitbox" @click.prevent="hit(12)"><component :is="pos12"></component></span>
-          </div>
+        <div style="display: flex;" class="row">
+          <span class="hitbox" @click.prevent="hit(1)">
+            <component :is="pos1"></component>
+          </span>
+          &nbsp;
+          <span class="hitbox" @click.prevent="hit(2)">
+            <component :is="pos2"></component>
+          </span>
+          &nbsp;
+          <span class="hitbox" @click.prevent="hit(3)">
+            <component :is="pos3"></component>
+          </span>
+          &nbsp;
+          <span class="hitbox" @click.prevent="hit(4)">
+            <component :is="pos4"></component>
+          </span>
+        </div>
+        <div style="display: flex;" class="row">
+          <span class="hitbox" @click.prevent="hit(5)">
+            <component :is="pos5"></component>
+          </span>
+          &nbsp;
+          <span class="hitbox" @click.prevent="hit(6)">
+            <component :is="pos6"></component>
+          </span>
+          &nbsp;
+          <span class="hitbox" @click.prevent="hit(7)">
+            <component :is="pos7"></component>
+          </span>
+          &nbsp;
+          <span class="hitbox" @click.prevent="hit(8)">
+            <component :is="pos8"></component>
+          </span>
+        </div>
+        <div style="display: flex;" class="row">
+          <span class="hitbox" @click.prevent="hit(9)">
+            <component :is="pos9"></component>
+          </span>
+          &nbsp;
+          <span class="hitbox" @click.prevent="hit(10)">
+            <component :is="pos10"></component>
+          </span>
+          &nbsp;
+          <span class="hitbox" @click.prevent="hit(11)">
+            <component :is="pos11"></component>
+          </span>
+          &nbsp;
+          <span class="hitbox" @click.prevent="hit(12)">
+            <component :is="pos12"></component>
+          </span>
+        </div>
       </div>
       <br>
     </div>
-    <div style="margin-left: 2rem; margin-right: 2rem; display: flex; justify-content: space-between;">
+    <div
+      style="margin-left: 2rem; margin-right: 2rem; display: flex; justify-content: space-between;"
+    >
       <span>
         <a href="#" @click.prevent="startstop" v-if="statusAdmin"><i>START</i></a> ({{startStatus}})<br><br>
         <span style="font-size: 1.8rem;">
@@ -65,12 +91,12 @@
 </template>
 
 <script>
-import db from '@/api/api';
+import db from "@/api/api";
 
-import d3 from '@/components/d3.vue';
-import d2 from '@/components/d2.vue';
-import d1 from '@/components/d1.vue';
-import d0 from '@/components/nd.vue';
+import d3 from "@/components/d3.vue";
+import d2 from "@/components/d2.vue";
+import d1 from "@/components/d1.vue";
+import d0 from "@/components/nd.vue";
 
 let refresh;
 let countdown;
@@ -80,22 +106,25 @@ export default {
     return {
       thisRoom: {},
       myPoint: 0,
-      position: 'none',
+      position: "none",
       elevation: 0,
       myscore: 0,
       start: false,
       timeleft: 30000,
-      alert: '',
-      statusAdmin: null,
+      alert: "",
+      statusAdmin: null
     };
   },
   components: {
-    d3, d2, d1, d0,
+    d3,
+    d2,
+    d1,
+    d0
   },
   created() {
-    db.collection('room')
+    db.collection("room")
       .doc(this.$route.params.id)
-      .onSnapshot((doc) => {
+      .onSnapshot(doc => {
         console.log(doc.data());
         this.thisRoom = doc.data();
         if (this.thisRoom.players[0].name === localStorage.username) {
@@ -112,7 +141,7 @@ export default {
           }, 500);
         }
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
       });
   },
@@ -120,38 +149,44 @@ export default {
     hit(num) {
       console.log(`${num}  |  ${this.position}`);
       if (num === this.position) {
+        var audio = new Audio("/hit.mp3");
+        audio.play();
         console.log(`Hit pos ${num}`);
         this.myscore += 1;
-        this.alert = 'Hit !!!';
+        this.alert = "Hit !!!";
         setTimeout(() => {
-          this.alert = '';
+          this.alert = "";
         }, 500);
 
         const newPlayers = this.thisRoom.players;
-        newPlayers.find((element) => {
+        newPlayers.find(element => {
           if (element.name === localStorage.username) {
             element.score = this.myscore;
           }
         });
 
-        db.collection('room')
+        db.collection("room")
           .doc(this.$route.params.id)
           .update({
-            players: newPlayers,
+            players: newPlayers
           })
           .then(() => {
-            console.log('success ');
+            console.log("success ");
           })
-          .catch((err) => {
+          .catch(err => {
             console.log(err);
           });
+      } else {
+        var audio = new Audio("/miss.mp3");
+        audio.play();
       }
     },
+
     startstop() {
-      db.collection('room')
+      db.collection("room")
         .doc(this.$route.params.id)
         .update({
-          status: true,
+          status: true
         })
         .then();
     },
@@ -163,7 +198,7 @@ export default {
       } else {
         return "Not yet Started"
       }
-      return 'Not yet Started';
+      return "Not yet Started";
     },
     formatTimeLeft: function () {
       return (this.timeleft / 1000).toFixed(1)
@@ -172,74 +207,74 @@ export default {
       if (this.position === 1) {
         return 'd' + this.elevation;
       }
-      return 'd0';
+      return "d0";
     },
     pos2: function () {
       if (this.position === 2) {
         return 'd' + this.elevation;
       }
-      return 'd0';
+      return "d0";
     },
     pos3: function () {
       if (this.position === 3) {
         return 'd' + this.elevation;
       }
-      return 'd0';
+      return "d0";
     },
     pos4: function () {
       if (this.position === 4) {
         return 'd' + this.elevation;
       }
-      return 'd0';
+      return "d0";
     },
     pos5: function () {
       if (this.position === 5) {
         return 'd' + this.elevation;
       }
-      return 'd0';
+      return "d0";
     },
     pos6: function () {
       if (this.position === 6) {
         return 'd' + this.elevation;
       }
-      return 'd0';
+      return "d0";
     },
     pos7: function () {
       if (this.position === 7) {
         return 'd' + this.elevation;
       }
-      return 'd0';
+      return "d0";
     },
     pos8: function () {
       if (this.position === 8) {
         return 'd' + this.elevation;
       }
-      return 'd0';
+      return "d0";
     },
     pos9: function () {
       if (this.position === 9) {
         return 'd' + this.elevation;
       }
-      return 'd0';
+      return "d0";
     },
     pos10: function () {
       if (this.position === 10) {
         return 'd' + this.elevation;
       }
-      return 'd0';
+      return "d0";
     },
     pos11: function () {
       if (this.position === 11) {
         return 'd' + this.elevation;
       }
-      return 'd0';
+      return "d0";
     },
     pos12: function () {
       if (this.position === 12) {
         return 'd' + this.elevation;
       }
-      return 'd0';
-    },
+      return "d0";
+    }
   },
   watch: {
     position() {
@@ -269,33 +304,33 @@ export default {
         clearInterval(countdown);
         this.$router.push(`/room/${this.$route.params.id}/result`);
       }
-    },
-  },
+    }
+  }
 };
 </script>
 
 <style scoped>
 #room {
-    all: initial;
+  all: initial;
 }
 
 * {
-    color: white;
-    font-family: 'Inconsolata', monospace;
-    margin: 0;
-    padding: 0;
+  color: white;
+  font-family: "Inconsolata", monospace;
+  margin: 0;
+  padding: 0;
 }
 
 #room {
-    width: 100%;
-    height: 95vh;
+  width: 100%;
+  height: 95vh;
 }
 
 .diglet {
-    font-size: 0.9rem;
-    font-weight: 500;
-    background-color: rgba(0, 0, 0, 0);
-    margin: 1.2rem;
+  font-size: 0.9rem;
+  font-weight: 500;
+  background-color: rgba(0, 0, 0, 0);
+  margin: 1.2rem;
 }
 
 .hitbox {
@@ -303,7 +338,7 @@ export default {
 }
 
 #moles {
-    top: 1rem;
-    z-index: 1;
+  top: 1rem;
+  z-index: 1;
 }
 </style>
